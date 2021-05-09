27840f39d61a50c7687a50b583e86343a36c9423
Simon Voordouw <simonvoordouw@gmail.com>
2021-05-09 23:41:16 +0200

    build webassembly target with nix

--- build_web.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-cargo build --target "wasm32-unknown-unknown" --features web
-wasm-bindgen --out-dir target --out-name wasm --target web --no-typescript target/wasm32-unknown-unknown/debug/game-of-life-bevy.wasm
--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    wasm-bindgen-legacy.url = "github:NixOS/nixpkgs?rev=9bb3fccb5b55326cb3c2c507464a8a28d44d1730";
+    wasm-bindgen-269.url = "github:NixOS/nixpkgs?rev=9bb3fccb5b55326cb3c2c507464a8a28d44d1730";
     utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
     naersk.url = "github:nmattia/naersk";
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk, wasm-bindgen-legacy, ... }:
+  outputs = { self, nixpkgs, utils, rust-overlay, naersk, wasm-bindgen-269, ... }:
     utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        rust = pkgs.rust-bin.stable.latest.default.override {
-                extensions = [ "rust-src" ];
-                targets = [ "wasm32-unknown-unknown" ];
-              };
+        rust = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ 
+          overlays = [
             rust-overlay.overlay
             (self: super: {
               rustc = rust;
               cargo = rust;
-              wasm-bindgen-cli = (import wasm-bindgen-legacy { inherit system; }).wasm-bindgen-cli;
+              wasm-bindgen-cli = (import wasm-bindgen-269 { inherit system; }).wasm-bindgen-cli;
             })
           ];
         };
@@ -31,21 +31,40 @@
         };
       in
       rec {
-        packages.game-of-life = naersk-lib.buildPackage {
-          pname = "game-of-life";
-          root = ./.;
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            alsaLib
-            libudev
-            xorg.libX11
-            xlibs.libX11
-            xlibs.libXcursor
-            xlibs.libXi
-            xlibs.libXrandr
-            python3
-          ];
-        };
+        packages =
+          rec {
+            game-of-life = naersk-lib.buildPackage {
+              pname = "game-of-life";
+              cargoBuildOptions = (old: old ++ [ "--features web" ]);
+              CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+              root = ./.;
+              nativeBuildInputs = with pkgs; [
+                pkg-config
+                alsaLib
+                libudev
+                xorg.libX11
+                xlibs.libX11
+                xlibs.libXcursor
+                xlibs.libXi
+                xlibs.libXrandr
+                python3
+              ];
+            };
+            game-of-life-web = pkgs.stdenv.mkDerivation {
+              name = "game-of-life-web";
+              nativeBuildInputs = with pkgs; [
+                wasm-bindgen-cli
+                game-of-life.overrideAttr (attrs: {
+                })
+              ];
+              phases = [ "installPhase" ];
+              installPhase = ''
+                mkdir -p $out
+                wasm-bindgen --out-dir $out --out-name wasm --target web --no-typescript ${game-of-life}/bin/game-of-life-bevy.wasm
+              '';
+            };
+          };
+
         defaultPackage = packages.game-of-life;
 
         apps.game-of-life = utils.lib.mkApp {
@@ -55,13 +74,13 @@
 
         devShell = pkgs.mkShell {
           inputsFrom = [ packages.game-of-life ];
-          RUST_SRC_PATH="${pkgs.rust-bin.stable.latest.rust-src}/lib/rustlib/src/rust/library/";
-          buildInputs = with pkgs; [ 
+          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.rust-src}/lib/rustlib/src/rust/library/";
+          buildInputs = with pkgs; [
             rust-bin.stable.latest.default
             cargo-edit
             wasm-bindgen-cli
             simple-http-server
-            vulkan-loader 
+            vulkan-loader
             lldb
           ];
         };
