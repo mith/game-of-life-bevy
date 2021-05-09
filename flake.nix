71ef5390c37147e1e10663c842997d1050c51a7e
Simon Voordouw <simonvoordouw@gmail.com>
2021-05-10 01:51:43 +0200

    push build to github pages

--- flake.nix
+++ flake.nix
@@ -10,7 +10,11 @@
   outputs = { self, nixpkgs, utils, rust-overlay, naersk, wasm-bindgen-269, ... }:
     utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        rust = pkgs.rust-bin.nightly.latest.default.override {
+        rust-nightly = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        rust-stable = pkgs.rust-bin.nightly.latest.default.override {
           extensions = [ "rust-src" ];
           targets = [ "wasm32-unknown-unknown" ];
         };
@@ -19,15 +23,15 @@
           overlays = [
             rust-overlay.overlay
             (self: super: {
-              rustc = rust;
-              cargo = rust;
+              rustc = rust-stable;
+              cargo = rust-stable;
               wasm-bindgen-cli = (import wasm-bindgen-269 { inherit system; }).wasm-bindgen-cli;
             })
           ];
         };
         naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
+          cargo = rust-nightly;
+          rustc = rust-stable;
         };
       in
       rec {
@@ -35,8 +39,6 @@
           rec {
             game-of-life = naersk-lib.buildPackage {
               pname = "game-of-life";
-              cargoBuildOptions = (old: old ++ [ "--features web" ]);
-              CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
               root = ./.;
               nativeBuildInputs = with pkgs; [
                 pkg-config
@@ -50,19 +52,27 @@
                 python3
               ];
             };
-            game-of-life-web = pkgs.stdenv.mkDerivation {
-              name = "game-of-life-web";
-              nativeBuildInputs = with pkgs; [
-                wasm-bindgen-cli
-                game-of-life.overrideAttr (attrs: {
-                })
-              ];
-              phases = [ "installPhase" ];
-              installPhase = ''
-                mkdir -p $out
-                wasm-bindgen --out-dir $out --out-name wasm --target web --no-typescript ${game-of-life}/bin/game-of-life-bevy.wasm
-              '';
-            };
+            game-of-life-web =
+              let
+                game-of-life-wasm = (game-of-life.overrideAttrs (attrs: {
+                  cargo_build_options = attrs.cargo_build_options ++ [ "--features web" ];
+                  CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+                }));
+              in
+              pkgs.stdenv.mkDerivation {
+                name = "game-of-life-web";
+                src = ./.;
+                nativeBuildInputs = with pkgs; [
+                  wasm-bindgen-cli
+                  game-of-life-wasm
+                ];
+                phases = [ "unpackPhase" "installPhase" ];
+                installPhase = ''
+                  mkdir -p $out
+                  wasm-bindgen --out-dir $out --out-name wasm --target web --no-typescript ${game-of-life-wasm}/bin/game-of-life-bevy.wasm
+                  cp index.html $out/index.html
+                '';
+              };
           };
 
         defaultPackage = packages.game-of-life;
